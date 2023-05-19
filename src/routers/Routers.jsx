5cafb4d9bdc411001de7f7302e8293d88a3d87f2
hasan <hasankanj2000@gmail.com>
2023-05-20 01:05:24 +0300

    done

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -9,6 +9,7 @@ import Home from "../Components/Home/Home";
 import Contactus from "../Components/ContactUs/Contactus";
 import Page404 from "../Components/404 Page/Page404";
 import List from "../Components/list/List";
+import Hotel from "../Components/hotel/Hotel";
 const Routers = () => {
   return (
     <div className="App">
@@ -21,6 +22,7 @@ const Routers = () => {
           <Route path="/About" element={<About />} />
           <Route path="/register" element={<Register />} />
           <Route path="/hotels" element={<List />} />
+          <Route path="/hotels/:id" element={<Hotel />} />
           <Route path="/*" element={<Page404 />} />
       </Routes>
       <ScrollToTop />
